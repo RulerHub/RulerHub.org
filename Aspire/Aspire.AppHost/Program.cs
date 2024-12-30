04831de257291c65f59fe1d471db34e7aaef7067
Adrian Mesa Sacasas <adrianmesasacasas@outlook.com>
2024-12-30 13:03:44 -0500

    Revirtiendo eStore corrigiendo error en FluentIcons y en Theme

--- Aspire/Aspire.AppHost/Program.cs
+++ Aspire/Aspire.AppHost/Program.cs
@@ -1,17 +1,8 @@
-using Projects;
-
 var builder = DistributedApplication.CreateBuilder(args);
 
-var eStoreApi = builder.AddProject<Projects.eStore_Api>("eStoreApi");
-
-builder.AddProject<Projects.RulerHub>("RulerHub")
-    .WithExternalHttpEndpoints()
-    .WithReference(eStoreApi)
-    .WaitFor(eStoreApi);
-
-builder.AddProject<Projects.eStore_Web>("eStoreWeb")
-    .WithExternalHttpEndpoints()
-    .WithReference(eStoreApi)
-    .WaitFor(eStoreApi);
+builder.AddProject<Projects.RulerHub>("RulerHub");
+//.WithExternalHttpEndpoints()
+//.WithReference(eStoreApi)
+//.WaitFor(eStoreApi)
 
 builder.Build().Run();
--- Server/eStore.Api/DataTransferObjets/ProductDtos/UpdateProductDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using eStore.Api.Entities;
-
-namespace eStore.Api.DataTransferObjets.ProductDtos;
-
-public class UpdateProductDto
-{
-    public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-}
