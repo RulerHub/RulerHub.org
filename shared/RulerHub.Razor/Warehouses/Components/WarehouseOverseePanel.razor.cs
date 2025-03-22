b9ce4bf25a56dbd126bac414fcbba238ce9815ab
Adrian Mesa Sacasas <adrianmesasacasas@outlook.com>
2025-03-22 17:14:13 -0400

    refact project

---
+++ shared/RulerHub.Razor/Warehouses/Components/WarehouseOverseePanel.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+using RulerHub.Shared.Entities.Warehouses;
+
+namespace RulerHub.Razor.Warehouses.Components;
+
+public partial class WarehouseOverseePanel : IDialogContentComponent<Warehouse>
+{
+    [Parameter]
+    public Warehouse Content { get; set; } = default!;
+
+    private void GotoItemManage()
+    {
+        NavigationManager.NavigateTo("item/home");
+    }
+}
---
+++ shared/RulerHub.Razor/Warehouses/Components/WarehouseUpdateForm.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.FluentUI.AspNetCore.Components;
+using RulerHub.Shared.Entities.Warehouses;
+
+namespace RulerHub.Razor.Warehouses.Components;
+
+public partial class WarehouseUpdateForm
+{
+    private EditContext _editContext = default!;
+    [CascadingParameter]
+    public FluentDialog Dialog { get; set; } = default!;
+
+    [Parameter]
+    public Warehouse Content { get; set; } = default!;
+
+    protected override void OnInitialized()
+    {
+        _editContext = new EditContext(Content);
+    }
+
+    private async Task CancelAsync()
+    {
+        ToastService.ShowWarning("Create cancel");
+        await Dialog.CancelAsync();
+    }
+
+    private async Task SaveAsync()
+    {
+        if (_editContext.Validate())
+        {
+            ToastService.ShowSuccess("Update Warehouse");
+            await WarehouseService.UpdateAsync(Content.Id, Content);
+            await Dialog.CloseAsync(Content);
+        }
+    }
+}
