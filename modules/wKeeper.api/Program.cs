4ad740b914d23fb27b13c5ee7b934862fd07b866
Adrian Mesa Sacasas <adrianmesasacasas@outlook.com>
2025-03-26 18:47:28 -0400

    Añadir configuraciones y servicios para RulerHub
    Se corrigieron errores que impedian la utilizacion de servicios por parte de la api
    
    Se han añadido nuevos servicios en `Program.cs`, incluyendo infraestructura y acceso al contexto HTTP. Se ha configurado el pipeline de solicitudes para el entorno de desarrollo.
    
    En `appsettings.Development.json`, se ha añadido una cadena de conexión para la base de datos local de SQL Server.
    
    Se ha actualizado la dirección del host de la API en `wKeeper.api.http` de `http://localhost:5192` a `https://localhost:7283` y se ha modificado la solicitud GET para un nuevo endpoint de proveedor.
    
    Se ha eliminado la configuración de la cadena de conexión en `appsettings.json`, indicando que ahora se utiliza `appsettings.Development.json` para la conexión a la base de datos.

--- modules/wKeeper.api/Program.cs
+++ modules/wKeeper.api/Program.cs
@@ -1,12 +1,18 @@
+using RulerHub.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddHttpContextAccessor(); 
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
